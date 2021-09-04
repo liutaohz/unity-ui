9724ea6c6021e307700d12f16ee15a7ca3b54c03
liutao <liutaohangzhou@163.com>
2021-09-04 12:49:53 +0800

    style: 🎨 文档样式优化
    配置logo,优化文档样式

--- .umirc.ts
+++ .umirc.ts
@@ -3,9 +3,11 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'unity-ui',
   description: '小而美的UI组件库',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: './unity-ui.png',
+  logo: './logo.png',
+  // favicon:
+  //   'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  // logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   // mode:'site',
   outputPath: 'docs-dist',
   history: {
@@ -22,5 +24,21 @@ export default defineConfig({
     },
   ],
   publicPath: '/unity-ui/',
+  styles: [
+    `
+      .__dumi-default-menu-header h1 {
+        display: none;
+      }
+      .__dumi-default-menu-logo {
+        width: 150px !important;
+      }
+      @media only screen and (max-width: 767px) {
+        .__dumi-default-navbar-logo:not([data-plaintext]) {
+          padding: 0 50px;
+          color: transparent;
+        }
+      }
+    `,
+  ],
   // more config: https://d.umijs.org/config
 });
